19ea2716b6b22ef825be3b51fac2c5a72c49625d
Nitaicandra <89361982+Nitaicandra@users.noreply.github.com>
2022-09-17 06:59:08 -0400

    Working Google translate + Hugging face roBerta sentiment analysis model
    translates greek to english then performs sentiment analysis using a roBERTa model from https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest?text=Covid+cases+are+increasing+fast%21

---
+++ src/SentimentAnalysisTest/test.py
@@ -0,0 +1,30 @@
+#import
+from googletrans import Translator
+import numpy as np
+import pandas as pd 
+from transformers import pipeline
+
+#encodeing must = utf8
+f= open('GreekList.txt', 'r',encoding='utf8') 
+source = f.readlines()
+f.close()
+
+#remove /n from strings
+for index,item in enumerate(source): 
+    source[index]=item.strip()
+    
+#initialize class
+translator = Translator()
+#call googltranlate for each entry in lines
+translated = translator.translate(source,src='el',dest='en')
+
+#print 
+'''
+for t in translated:
+    print(f'{t.origin} -> {t.text}')
+'''
+
+sentiment_task = pipeline("sentiment-analysis", model='cardiffnlp/twitter-roberta-base-sentiment-latest', 
+tokenizer='cardiffnlp/twitter-roberta-base-sentiment-latest')
+for t in translated:
+    print(f'{t.origin}  ->  {t.text}  ->    {sentiment_task(t.text)}\n')
