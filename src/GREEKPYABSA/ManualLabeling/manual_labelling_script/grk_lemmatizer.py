d13a8611652a04c5af8d93e838e97e1b3864605e
Aarushi Jain <115092237+ashij07@users.noreply.github.com>
2024-03-25 13:34:54 -0400

    Add files via upload
    gui wrapper function files

---
+++ src/GREEKPYABSA/ManualLabeling/manual_labelling_script/grk_lemmatizer.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+GRK_WORDS_CSV = "greek_words.csv"
+
+#load in the dataframes for functions
+lemmaDF = pd.read_csv(GRK_WORDS_CSV, sep = "\t")
+del lemmaDF['Unnamed: 0']
+
+#outputs list of matching IDs or an empty list
+def get_id(word):
+    #search the lemmaDF for the word
+    word = word.lower()
+    output = []
+    #this is the dataframe of all rows with the word
+    df = lemmaDF.loc[lemmaDF["bare_text"] == word]
+        #iterrows works, but the others dont. ok whatever
+    for index, row in df.iterrows():
+        if(row["id"] not in output):
+            output.append(row["id"])
+    return(output)
---
+++ src/GREEKPYABSA/ManualLabeling/manual_labelling_script/gui_wrapper.py
@@ -0,0 +1,64 @@
+# Write wrapping function so that, if someone wants to use the GUI on a particular word, 
+# it will call use dictionary function on CompleteCSVs, not post-results, then grab the
+# full dataframe, strip it down to just text section, unindexed and without header, and 
+# remove numbers from the text (clean a little)
+# CSV Data should be single column without header
+
+import re
+import pandas as pd
+import pickle
+import grk_lemmatizer
+
+
+def gui_wrapper(query_word, pkl_file, csv_file):
+
+    #call query_data function to get the initial dataframe
+    df = query_data(query_word, pkl_file, csv_file)
+
+    #strip the dataframe to just text section
+    text_df = df["Text Section"].values
+    text_df = pd.DataFrame(text_df)
+
+    #remove numbers from the text
+    text_df = text_df.applymap(lambda x: re.sub(r'[\d\.\t\n]', '', str(x)))
+
+    return text_df
+
+
+def query_data(query_word, input_file, data_table):
+
+    #place contents of pickle file into dictionary container 
+    with open(input_file, 'rb') as file:
+        dictionary = pickle.load(file)
+
+    ids_from_word = grk_lemmatizer.get_id(query_word)  #transform word to list of ids with lemmatizer
+
+    data_table = pd.read_csv(data_table, delimiter=',', header = 0)  #load csv file into dataframe
+
+    final_table = pd.DataFrame()   #create empty dataframe
+
+    #go through each id and get row
+    for id_from_word in ids_from_word:
+        if id_from_word in dictionary:
+            #print(f"This is the id: {id_from_word}")
+                                
+            #get row index and then get the entire row based on the index
+            row_index = dictionary[id_from_word]
+            dataframe_row = data_table.iloc[row_index]
+                
+            #print(dataframe_row)
+                
+            #append row to final table
+            final_table = pd.concat([final_table, pd.DataFrame(dataframe_row)], ignore_index=True)
+
+        else: 
+            print(f"{query_word} doesn't exist.")
+            return -1
+
+    return final_table
+
+
+
+# gui_word = "φιλια"
+# output_df = gui_wrapper(gui_word, "dictionary_grk.pkl", "0-50kgreekv1.csv")
+# print(output_df)
