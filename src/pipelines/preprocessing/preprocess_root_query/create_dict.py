e39e5bd693858aaa87bca3477bba707efd39250a
Austin-Stein <austin.stein@ufl.edu>
2024-03-27 20:16:56 -0400

    Added Connor's updated create_dict changes which includes stripping accents

--- src/pipelines/preprocessing/preprocess_root_query/create_dict.py
+++ src/pipelines/preprocessing/preprocess_root_query/create_dict.py
@@ -1,9 +1,11 @@
 from enum import Enum
-import csv
+#import csv
+import pandas
 import sys
 import pickle
 import argparse
 import os
+import unicodedata
 
 
 import grk_lemmatizer
@@ -11,7 +13,9 @@ import ltn_lemmatizer
 
 
 GRK_CSV = "0-50kgreekv1.csv"
+GRK_CSV_COMPLETE = "CompleteCSVForGreek.csv"
 LTN_CSV = ""
+LTN_CSV_COMPLETE = "CompleteCSVForLatin.csv"
 
 
 class Language(Enum):
@@ -34,24 +38,32 @@ def exit():
     sys.exit()
     
 
-def load_csv(lang: Language) -> csv.reader:
+def load_csv(lang: Language, complete: bool) -> pandas.DataFrame:#csv.reader:
     file_name = ""
 
     if lang == Language.GRK:
-        file_name = GRK_CSV
+        if complete:
+            file_name = GRK_CSV_COMPLETE
+        else:
+            file_name = GRK_CSV
     elif lang == Language.LTN:
-        file_name = LTN_CSV
+        if complete:
+            file_name = LTN_CSV_COMPLETE
+        else:
+            file_name = LTN_CSV
 
     # Open CSV file
     try:
         file_obj = open(f"{file_name}", 'r', encoding='utf-8')
-        reader_obj = csv.reader(file_obj)
+        #reader_obj = csv.reader(file_obj)
+        df = pandas.read_csv(file_obj)
         print("File ", file_name, " opened successfully.")
     except IOError:
         print("Failed to open file: ", file_name)
         exit()
 
-    return reader_obj
+    #return reader_obj
+    return df
 
 
 # Old function, tokenizes all possible phrase combinations
@@ -69,21 +81,34 @@ def load_csv(lang: Language) -> csv.reader:
     #return aspect_tokens
 
 
-def init_dict(lang: Language, force = None, limit = None):
+def init_dict(lang: Language, force = None, limit = None, complete = False):
+
+    # Helper function for the complete CSVs
+    # Removes non-alpha chars while preserving accents, diacritics, etc.
+    def remove_non_alpha(row):
+        s = ' '.join(row.splitlines())
+        #return ''.join(char for char in row if unicodedata.category(char)[0] in {'L', 'M', 'Z'})
+        return ''.join(char for char in s if unicodedata.category(char)[0] in {'L', 'M', 'Z'})
+    
+    def strip_accents(string): 
+        return "".join(c for c in unicodedata.normalize("NFD", string) if not unicodedata.combining(c)) 
 
     # Make sure we're not overwriting an existing dictionary
     # if exists -> error
 
     suffix = 'grk' if lang == Language.GRK else 'ltn'
+    if complete: suffix = suffix + '_complete'
     dict_filename = f"dictionary_{suffix}.pkl"
+
     if os.path.isfile(dict_filename):
         if not force:
             print("Pickled dictionary file already exists. Delete it and rerun or run with --force.")
             exit()
 
-    csv_reader = load_csv(lang)
+    #csv_reader = load_csv(lang, complete)
+    df = load_csv(lang, complete)
 
-    # CSV Format:
+    # CSV (Incomplete) Format:
     # ['', 'Unnamed: 0.1', 'Unnamed: 0', 'Section ID', 
     # 'Text Section', 'Title', 'Author', 'Translated Text',
     # 'TreePath', 'aspect', 'confidence', 'sentiment'] 
@@ -92,21 +117,45 @@ def init_dict(lang: Language, force = None, limit = None):
     dictionary = dict()
     
     # Skip first row of column headers
-    next(csv_reader)
-
+    #next(csv_reader)
 
     i = 0 # Index rows starting at 0 for the first data entry
-    for row in csv_reader:
+    
+    col_header = ''
+    if complete:
+        col_header = 'Text Section'
+    else:
+        col_header = 'aspect'
+
+    #for row in csv_reader:
+    #for row in list(df[col_header]): # convert to list for faster iteration (maybe?)
+    for n, r in df.iterrows():
+        row = r[col_header]
 
         # if i >= 100: break
 
         if limit:
-            if i >= limit: break
+            if i >= limit: 
+                break
 
         print(i)
 
-        aspect = row[9]
-        words = aspect.split()
+        if complete:
+            #aspects = row['Text Section']
+            #words = remove_non_alpha(aspects).split()
+            text = remove_non_alpha(row)
+            
+            if lang == Language.GRK:
+                text = strip_accents(text)
+
+            words = text.split()
+        else:
+            #aspect = row['aspect']
+            #words = aspect.split()
+            words = row.split()
+
+        print(words)
+        
         ids = []
         for word in words:
             if lang == Language.GRK:
@@ -120,10 +169,10 @@ def init_dict(lang: Language, force = None, limit = None):
             if id not in dictionary:
                 dictionary[id] = [i]
             else:
-                dictionary[id].append(i)
+                if i not in dictionary[id]:
+                    dictionary[id].append(i)
 
         i += 1
-
 
     print(dictionary)
 
@@ -155,6 +204,7 @@ def main():
     parser.add_argument("-l", action='store_true')
     parser.add_argument("--force", required=False, action='store_true')
     parser.add_argument("--limit", required=False, type=int)
+    parser.add_argument("--complete", required=False, default=None, action='store_true')
     # Consider adding more args for specifying I/O file names
 
     args = parser.parse_args()
@@ -162,8 +212,9 @@ def main():
     lang = None
     force = args.force
     limit = None
-    if args.limit: limit = args.limit
-    
+    if args.limit: limit = args.limit # Default: None
+
+    complete = args.complete
 
     if args.g and not args.l:
         lang = Language.GRK
@@ -181,7 +232,20 @@ def main():
             else: 
                 choice = None
 
-    init_dict(lang, force, limit)
+    if complete is None:
+        choice = None
+        while choice is None:
+            choice = input("Choose:\n\t1. Partial CSV\n\t2. Complete CSV\n")
+            
+            if choice == '1': 
+                complete = False
+            elif choice == '2': 
+                complete = True
+            else: 
+                choice = None
+
+
+    init_dict(lang, force, limit, complete)
 
     exit()
 
